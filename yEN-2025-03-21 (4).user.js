aa9f48678d4ebc3e4f417c9f41801486cd01ba16
fatihtekar03960-hub <fatihtekar03960@gmail.com>
2025-09-14 19:21:21 +0300

    criss

--- yEN-2025-03-21 (4).user.js
+++ yEN-2025-03-21 (4).user.js
@@ -8,6 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=agarz.com
 // @grant        none
 // ==/UserScript==
+const url = "https://kullaniciadi.github.io/betik-lisans/license.json";
 
 
 (function() {
